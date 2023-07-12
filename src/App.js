332d3c6c39373f9a888cc37dadd7820da456ee3d
dilaerismaseptiana <dlarsmsptna7743@gmail.com>
2023-07-12 07:28:14 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-import ContactPage from './ContactPage';
+import TambahData from './TambahData';
+import Login from './Login';
+import Register from './register'
+import AddPersonForm from './AddPersonForm';
 import './styles.css';
 import './form.css';
 
@@ -12,30 +13,32 @@ const App = () => {
     <Router>
       <div className="app">
         <header>
-          <h1>World Animal</h1>
+          <h1>The Best Campus in Jogja</h1>
           <nav>
             <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
               <li>
                 <Link to="/dashboard">Dashboard</Link>
               </li>
               <li>
-                <Link to="/about">About</Link>
+                <Link to="/tambahdata">Add Data</Link>
+              </li>
+              {/* <li>
+                <Link to="/login">Login</Link>
               </li>
               <li>
-                <Link to="/contact">Contact</Link>
-              </li>
+                <Link to="/register">Sign Up</Link>
+              </li> */}
             </ul>
           </nav>
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/tambahdata" element={<TambahData />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />}/>
+            {/* <Route exact path="/add-person" component={AddPersonForm} /> */}
           </Routes>
         </main>
       </div>
